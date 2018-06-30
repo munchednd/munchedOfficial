26f8e684e4f13c0e922fee7656a50d452e18071b
Rayyan Karim <rkarim0598@gmail.com>
2018-06-29 19:30:48 -0500

    Add bootstrap and set up React

---
+++ munchie/src/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+// import './Main.css'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
